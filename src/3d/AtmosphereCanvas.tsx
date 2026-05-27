b6bcab54630d234b757359e7247c190e9fa3d6db
Hosein Nematolahi <69923142+Hoseinnematolahi@users.noreply.github.com>
2026-05-27 19:43:44 +0330

    Update AtmosphereCanvas.tsx

--- src/3d/AtmosphereCanvas.tsx
+++ src/3d/AtmosphereCanvas.tsx
@@ -1,5 +1,12 @@
 import { Canvas } from '@react-three/fiber';
 
 export function AtmosphereCanvas() {
-  return <Canvas className="fixed inset-0 -z-10" camera={{ position: [0, 0, 2] }} />;
+  return (
+    <Canvas
+      className="fixed inset-0 -z-10"
+      camera={{ position: [0, 0, 2] }}
+    >
+      <ambientLight intensity={0.5} />
+    </Canvas>
+  );
 }
